f8b11dc84a9985496524d05826da8a285888eba3
ianstankosh <istankos@villanova.edu>
2020-03-25 15:30:55 -0400

    Revert "Delete fb_data.txt"
    This reverts commit 91ee215c

--- spotify_main.py
+++ spotify_main.py
@@ -28,7 +28,11 @@ def create_playlist():  # creates playlist and returns playlist id
 
     resp_json = resp.json()
 
-    return resp_json["id"]  # id of Spotify playlist
+    try:
+        resp_json_id = resp_json["id"]  # id of Spotify playlist
+        return resp_json_id
+    except KeyError:
+        print("Expired Spotify OAuth Token")
 
 
 def get_artist_id(search_query):  # this is going to take the output from fb_main
